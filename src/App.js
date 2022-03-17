b39b40b7697133a9d5c8f923bf247664ed642a67
Colton Watkins <watkincw@gmail.com>
2022-03-17 12:07:51 -0500

    React Day 3 - classwork

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+// import Demo from './components/Demo.js';
+// import StyledDemo from './components/StyledDemo';
+import EffectHook from './components/EffectHook';
+import EffectNew from './components/EffectNew';
+import { ToDoComponent } from './components/ToDoComponent';
+import MyFuncComponent from './components/MyFuncComponent';
+import CommonStyles from './components/CommonStyles';
+import Component1 from './components/Component1';
+import UseRefDemo from './components/UseRefDemo';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Hello world from React!</h1>
+			<UseRefDemo />
+			{/* <Component1 />
+			<CommonStyles />
+			<EffectHook />
+			<EffectNew />
+			<ToDoComponent value="James" />
+			<MyFuncComponent name="Mike" age={34} /> */}
 		</div>
 	);
 }
